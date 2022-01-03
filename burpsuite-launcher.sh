892a53e95a7e61a211305df0aad9a494921fad08
Devender <49671176+raoshaab@users.noreply.github.com>
2022-01-03 10:38:56 +0530

    Update burpsuite-launcher.sh

--- burpsuite-launcher.sh
+++ burpsuite-launcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 pw=$(pwd) 
 loader=$(ls|grep -i loader)
 burp=$(ls|grep -i pro)
@@ -8,6 +7,8 @@ burp=$(ls|grep -i pro)
 
 
 dir="java  -Dfile.encoding=utf-8 -javaagent:$pw/$loader -noverify -jar $pw/$burp" 
+
+
 echo '[Desktop Entry]
 Name=Burpsuite
 Encoding=UTF-8
@@ -20,4 +21,4 @@ Categories=03-webapp-analysis;03-06-web-application-proxies;
 X-Kali-Package=burpsuite
 StartupWMClass=burp-StartBurp' > ~/.local/share/applications/kali-burpsuite.desktop
 
-echo -e "Launcher is set \n Just rock !! ~~~~~~~~~~~~~~~~"
+echo -e "Launcher is set \n Just rock !! ~~~~~~~~~" 
