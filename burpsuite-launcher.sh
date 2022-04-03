17c948c3a60a8bf0dcf006fd99e765e36637016d
Devender <49671176+raoshaab@users.noreply.github.com>
2022-04-03 17:44:51 +0530

    Update burpsuite-launcher.sh

--- burpsuite-launcher.sh
+++ burpsuite-launcher.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+RED="\e[31m"
+GREEN="\e[32m"
+
 pw=$(pwd) 
 loader=$(ls|grep -v old|grep -i loader)
 burp=$(ls|grep -i pro)
@@ -22,3 +26,10 @@ X-Kali-Package=burpsuite
 StartupWMClass=burp-StartBurp' > ~/.local/share/applications/kali-burpsuite.desktop
 
 echo -e "Launcher is set \nJust rock !! ~~~~~~~~~" 
+
+
+echo -e "${RED}         +-----------+------------------+-----------------------+
+         | You     | are    | Ready   |   For     |    Lauch    |
+         ${GREEN}+-----------+------------------+-----------------------+
+         | Laucher is set ,  Now  just search  in   your  Menu  |
+         +-----------+------------------+-----------------------+"
