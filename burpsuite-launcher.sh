c2e24b3a57325efb71ea5cf055d44d1ba7d9b4c1
Devender <49671176+raoshaab@users.noreply.github.com>
2022-11-26 19:22:25 +0530

    Added check for GUI

--- burpsuite-launcher.sh
+++ burpsuite-launcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+banner='BurpSuite Launcher '
+
 
 RED="\e[31m"
 GREEN="\e[32m"
@@ -10,23 +12,30 @@ burp=$(ls|grep -i pro)
 
 
 
-dir="java  -Dfile.encoding=utf-8 -javaagent:$pw/$loader -noverify -jar $pw/$burp" 
+dir="java  -Dfile.encoding=utf-8 -javaagent:$pw/$loader -noverify -jar $pw/$burp"
+ 
+loc='~/.local/share/applications/'
+ls $loc >/dev/null 2>&1 
 
-
-echo '[Desktop Entry]
+if [[ $? == 0 ]]
+then
+cat > ~/.local/share/applications/kali-burpsuite.desktop << EOF 
+[Desktop Entry]
 Name=Burpsuite
 Encoding=UTF-8
-Exec=sh -c "'"$dir"'" %u
+Exec=sh -c "$dir" %u
 Icon=burp
 StartupNotify=false
 Terminal=false
 Type=Application
 Categories=03-webapp-analysis;03-06-web-application-proxies;
 X-Kali-Package=burpsuite
-StartupWMClass=burp-StartBurp' > ~/.local/share/applications/kali-burpsuite.desktop
-
-echo -e "Launcher is set \nJust rock !! ~~~~~~~~~" 
-
+StartupWMClass=burp-StartBurp
+EOFn
+else
+    echo -e "${RED}
+    echo "Can not Create Launcher in this system"&& exit
+fi
 
 echo -e "${RED}         +-----------+------------------+-----------------------+
          | You     | are    | Ready   |   For     |    Lauch    |
